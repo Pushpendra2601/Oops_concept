0c5bd9d0ca5bd3827359e83d7042659b194cb1a4
Pushpendra Vishwakarma <97514846+Pushpendra2601@users.noreply.github.com>
2024-01-27 20:39:23 +0530

    Add files via upload

---
+++ Component/Parent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Child from "./Child";
+
+export default function Parent(){
+
+    
+    
+    return(
+        <div>
+            <Child name="Pushpya" add="Kopare"/>
+        </div>
+    )
+}
